c9953815c62da5ca2b711dc45f98ccab4403d32b
RaymondXiao1991@hotmail.com <RaymondXiao1991@hotmail.com>
2018-05-22 15:46:07 +0800

    first commit

---
+++ Sort_test.go
@@ -0,0 +1,103 @@
+package FaceWriter
+
+import (
+	"testing"
+	"sort"
+	"time"
+	"text/tabwriter"
+	"os"
+	"fmt"
+)
+
+type StringSlice []string
+
+func (p StringSlice) Len() int           { return len(p) }
+func (p StringSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p StringSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func TestSlice(t *testing.T) {
+	names := []string{
+		"halo", "grace",
+	}
+
+	sort.Sort(StringSlice(names))
+	if StringSliceEqual(names, []string{"grace", "halo"}) {
+		t.Log("test case passed")
+	} else {
+		t.Log("test case failed")
+	}
+}
+
+func StringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+func TestArtist(t *testing.T) {
+	sort.Sort(byArtist(tracks))
+	printTracks(tracks)
+
+	sort.Sort(sort.Reverse(byArtist(tracks)))
+	printTracks(tracks)
+	/*
+	if StringSliceEqual(names, []string{"grace", "halo"}) {
+		t.Log("test case passed")
+	} else {
+		t.Log("test case failed")
+	}
+	*/
+
+}
---
+++ Sync_test.go
@@ -0,0 +1,74 @@
+package FaceWriter
+
+import (
+	"fmt"
+	"time"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+/*
+场景：在一个高并发的web服务器中，要限制IP的频繁访问。
+现模拟100个IP同时并发访问服务器，每个IP要重复访问1000次。每个IP三分钟之内只能访问一次。
+完成该过程，要求能成功输出 success:100
+*/
+
+type Ban struct {
+	visitIPs map[string]struct{}
+}
+
+func NewBan() *Ban {
+	return &Ban{visitIPs: make(map[string]struct{})}
+}
+
+//判断IP是否存在
+func (o *Ban) visit(ip string) bool {
+	mapMutex.Lock()
+	defer mapMutex.Unlock()
+	if _, ok := o.visitIPs[ip]; ok {
+		return true
+	}
+	o.visitIPs[ip] = struct{}{}
+	go o.invalidAfter3Min(ip)
+	return false
+}
+
+// 3分钟后ip失效, 从map中移除. 因此ip再次访问时便可正常访问
+func (o *Ban) invalidAfter3Min(ip string) {
+	time.Sleep(time.Minute * 3)
+	mapMutex.Lock()
+	visitIPs := o.visitIPs
+	delete(visitIPs, ip)
+	o.visitIPs = visitIPs
+	mapMutex.Unlock()
+}
+
+var mapMutex *sync.Mutex // mutex to avoid concurrent map writes
+
+func TestSync(t *testing.T) {
+	mapMutex = new(sync.Mutex)
+	var success int64
+	ban := NewBan()
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 100; j++ {
+			wg.Add(1)
+			ipEnd := j
+			go func() {
+				defer wg.Done()
+				ip := fmt.Sprintf("192.168.1.%d", ipEnd)
+				if !ban.visit(ip) {
+					atomic.AddInt64(&success, 1)
+				}
+			}()
+		}
+	}
+	wg.Wait()
+
+	if success == 100 {
+		t.Log("test case passed")
+	} else {
+		t.Log("test case failed")
+	}
+}
